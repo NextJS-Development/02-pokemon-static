e3ba762bdad70e6b4b0038ee4bed02df3bee7506
Brayan Garcia <bgarcia.95@outlook.com>
2023-04-10 15:23:54 -0600

    add next-ui button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,10 @@
+import { Button } from "@nextui-org/react";
 import { NextPage } from "next";
 
 const HomePage: NextPage = () => {
   return (
     <>
-      <h1>Hello World</h1>
+      <Button color="gradient">Hello</Button>
     </>
   );
 };
